11f7abb9da7cc463b0740acb8fb4c86d1f65fe4c
Pade <upwagner@gmail.com>
2013-09-13 21:43:54 +0200

    Offspring

---
+++ ez80.h
@@ -0,0 +1,13 @@
+#ifndef _EZ80_H_
+#define _EZ80_H_
+
+int readMemory2();
+char hex1in();
+void hex1out();
+char hex2in(void);
+void hex2out(char k);
+int hex4in();
+void readMemory(void);
+void read16Memory(void);
+
+#endif 
